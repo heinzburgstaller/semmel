c881ab0210e0f7b27d945b7111886ff0209002ee
Heinz Burgstaller <heinz.burgstaller@dynatrace.com>
2026-03-31 10:50:49 +0200

    Add start scripts

--- packages/kornspitz/vite.config.ts
+++ packages/kornspitz/vite.config.ts
@@ -4,6 +4,7 @@ import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
+import { nitro } from "nitro/vite"; 
 
 const config = defineConfig({
 	plugins: [
@@ -12,6 +13,7 @@ const config = defineConfig({
 		tailwindcss(),
 		tanstackStart(),
 		viteReact(),
+		nitro({ preset: "bun" }), 
 	],
 });
 
